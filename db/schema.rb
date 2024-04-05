bb6fa3ecddd9fb1753586440335bb13352892423
Marilyn Marquez <marilynmarquez@Marilyns-MacBook-Pro.local>
2024-04-05 13:13:52 -0400

    changed port from 3000 to 3001

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_27_031957) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_29_174000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "user_name"
+    t.string "role"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
