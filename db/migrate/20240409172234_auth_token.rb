58eeacb42e536d40a817312423e392194214e8f0
Carla Montano <cm.emprendiendo@gmail.com>
2024-04-09 12:30:41 -0500

    auth token

---
+++ db/migrate/20240409172234_auth_token.rb
@@ -0,0 +1,6 @@
+class AuthToken < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :auth_token, :string
+    add_index :users, :auth_token
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_27_031957) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_09_172234) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_27_031957) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "auth_token"
+    t.index ["auth_token"], name: "index_users_on_auth_token"
   end
 
 end
